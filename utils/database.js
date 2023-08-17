59209fe360b27cbbdb3a6dcd479fb3cefb4f5ba7
SaiSrinivas321 <srinivas.32194@gmail.com>
2023-08-17 13:32:03 +0530

    database connection

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "cdm",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
